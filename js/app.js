7e67ce6cb53bcda0ed47dd50bfbcdddece05da0b
erikarossani <yeinygonzalezcifuentes@gmail.com>
2017-03-25 10:19:56 -0500

    ceando estructura de html

---
+++ js/app.js
@@ -0,0 +1,19 @@
+var load = function(){
+
+};
+
+$(document).ready(load);
+
+var ingresarNumero= function(event){
+
+};
+
+
+var validarNumero = function(event) {
+
+};
+
+var ordenarNumero = function(event) {
+
+};
+
