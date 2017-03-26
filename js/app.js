4ebfa9746f01856f928a32e4b2d0fae65eea08c8
erikarossani <yeinygonzalezcifuentes@gmail.com>
2017-03-26 10:38:32 -0500

    haciendo funcionalidades

--- js/app.js
+++ js/app.js
@@ -1,15 +1,40 @@
 var load = function(){
+	$("#numero").keydown(validarNumero);
+	$("#btn-ingresar").click(ingresarNumero);
+
 
 };
 
 $(document).ready(load);
+    var array = [];
 
-var ingresarNumero= function(event){
+var ingresarNumero = function(event){
+  	event.preventDefault();  	//Evita que la acción predeterminada del evento no ocurra
+  	array.push(Number($("#numero").val())); // Añade el número ingresado por el usuario al final de la matriz 
+  		
+  	$("#numero").focus().val("");  //borra el valor del input luego de agregar
 
+  	visualizarNumero();
+};
+
+var visualizarNumero = function(){
+	if (array.length > 0){
+	    $("#mostrar-numero").text(array.join(" "));
+	} else{
+		$("mostrar-numero").text("Ingrese un número");
+	}		
 };
 
 
 var validarNumero = function(event) {
+    var ascii = event.keyCode;
+
+	if (ascii == 8 || (ascii >= 48 && ascii <= 57)) {
+		return true;
+	} else {
+		return false;
+	} //Permite solo el ingreso de números.
+
 
 };
 
